119d1fa331b7d5939168aae1c0ca462d0e101b76
Wolf-08 <alejandro.montecillo>
2020-11-18 19:55:34 -0600

    Notes about variables declaration

--- first_code.playground/Contents.swift
+++ first_code.playground/Contents.swift
@@ -1,4 +1,19 @@
 import UIKit
 
+/*
+Swift en su mayor parte es C asi
+que algunas cosas funcionan como tal
+*/
+
 var str = "Hello, playground"
-var welcomeMessage: String 
+
+/*
+ Para declarar esta variable se le puede asignar un valor
+ y swift sabra de que tipo es pero si no se le asigna nada
+ Se le pone el tipo de dato que es para que no marque errores
+ */
+var welcomeMessage: String
+
+welcomeMessage = "Hello"
+//Formato para crear variables en una sola linea
+var red, green, blue: Double
